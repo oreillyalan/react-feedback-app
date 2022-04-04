f4e64b00152d9707aba1a0795afc21c640f9129b
oreillyalan <oreillyalan132@gmail.com>
2022-04-04 17:14:00 +0100

    Added feedback form and onChange event handler

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 const App = () => {
     const [feedBack, setFeedback] = useState(FeedbackData)
@@ -20,6 +21,7 @@ const App = () => {
         <>
             <Header />
             <div className="container">
+                <FeedbackForm />
                 <FeedbackStats feedBack={feedBack} />
                 <FeedbackList feedBack={feedBack} handleDelete={deleteFeedback} />
             </div>
